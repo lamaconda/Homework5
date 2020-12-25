a911e9dea6924ce6c43fecbca444e445b46bc889
lamaconda <kennethoffi6.6@gmail.com>
2020-12-25 19:26:57 +0300

    Task 10 is Done!

---
+++ level_2/task_10/src/main/java/by/lamaconda/task_10/App.java
@@ -0,0 +1,9 @@
+package by.lamaconda.task_10;
+
+import by.lamaconda.task_10.service.ArrayOfThree;
+
+public class App {
+    public static void main(String[] args) {
+        ArrayOfThree.arrayList();
+    }
+}
---
+++ level_2/task_10/src/main/java/by/lamaconda/task_10/service/ArrayOfThree.java
@@ -0,0 +1,15 @@
+package by.lamaconda.task_10.service;
+
+import java.util.Arrays;
+
+public class ArrayOfThree {
+
+    private ArrayOfThree() {
+    }
+
+    public static void arrayList() {
+        int[] array = {4, 5 ,7};
+
+        System.out.println(Arrays.toString(array));
+    }
+}
